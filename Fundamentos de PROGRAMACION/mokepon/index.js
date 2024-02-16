d56fbb8e2f547ce2456944d08e824b96bd908ad3
leonel javier calleja hernandez <leocallejas565@gmail.com>
2024-02-16 17:01:13 -0500

    GUARDADO16/02/2024:4:20-ID JUGADOR EN EXPRESS

--- Fundamentos de PROGRAMACION/mokepon/index.js
+++ Fundamentos de PROGRAMACION/mokepon/index.js
@@ -1,11 +1,24 @@
 // INICIANDO EL SERVIDOR
 const express = require("express")
 
+class Jugador{
+    constructor(id){
+        this.id =id
+    }
 
+}
 const app = express()
 
-app.get("/", (req,res) => {
-res.send("HOLA XD")
+
+const jugadores = []
+app.get("/unirse", (req,res) => {
+    const id =`${Math.random()}`
+
+    const jugador = new Jugador(id)
+
+    jugadores.push(jugador)
+
+    res.send(id)
 })
 
 app.listen(8080, ()=>{

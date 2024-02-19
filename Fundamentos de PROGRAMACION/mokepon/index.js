677af8ebeb1bc2be8699924d3352df869a520d89
leonel javier calleja hernandez <leocallejas565@gmail.com>
2024-02-19 11:53:09 -0500

    GUARDADO19/02/2024:11:50-explicacion-de-el-servidor

--- Fundamentos de PROGRAMACION/mokepon/index.js
+++ Fundamentos de PROGRAMACION/mokepon/index.js
@@ -1,6 +1,7 @@
 // INICIANDO EL SERVIDOR
+// importamos a express con require("express")
 const express = require("express")
-
+// se cree una class QUE REPRESENTARA LA LISTA DE LOS JUGADORES
 class Jugador{
     constructor(id){
         this.id =id
@@ -9,9 +10,11 @@ class Jugador{
 }
 const app = express()
 
-
+// SE CREO UNA CLASE QUE VA A REPRESENTAR A UNA LISTA DE JUGADORES QUE AL UNIRSE UN NUEVO JUGADOR SE VA LLENANDO LA LISTA
 const jugadores = []
+
 app.get("/unirse", (req,res) => {
+    // se genera un numero random que estre sera el identificador de el jugador que este ingrese a la direccion "localhost:8080/unirse" CON const id =`${Math.random()}`
     const id =`${Math.random()}`
 
     const jugador = new Jugador(id)
